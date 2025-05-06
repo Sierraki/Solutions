c38c2c6ddc132a6cf4d0f33535b3944c5d2216e9
Sierraki <Sierrakiper@outlook.com>
2025-05-06 23:51:20 +0800

    20250506 2351

--- 草稿.py
+++ 草稿.py
@@ -1,3 +1,4 @@
-class Solution:
-    def queryString(self, s: str, n: int) -> bool:
-        return all(bin(i)[2:] in s for i in range(1, n + 1))
+import bisect
+from sortedcontainers import SortedList
+from collections import defaultdict, Counter
+
