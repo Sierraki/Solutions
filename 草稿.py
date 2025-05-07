d963ccdf10024eacdffe2434335ff6ba1ba3bf89
Sierraki <Sierrakiper@outlook.com>
2025-05-07 23:41:59 +0800

    20250507 2341

--- 草稿.py
+++ 草稿.py
@@ -1,17 +1,14 @@
-import bisect
-from sortedcontainers import SortedList
-from collections import defaultdict, Counter
+s = "aabccabba"
 
-nums = [1, 0, 1, 1, 0]
-
-cnt = defaultdict(int)
-
-left = mx = 0
-
-for idx, right in enumerate(nums):
-    cnt[right] += 1
-    while cnt[0] > 1:
-        cnt[nums[left]] -= 1
-        left += 1
-    mx = max(mx, idx - left + 1)
-    print(mx)
+n = len(s)
+left, right = 0, n - 1
+while left <= right:
+    while s[left] == s[right]:
+        if s[left] == s[left + 1]:
+            left += 1
+        if s[right] == s[right - 1]:
+            right -= 1
+    while s[left+1] == s[right-1]:
+        left+=1
+        right-=1
+    print(left, right)
