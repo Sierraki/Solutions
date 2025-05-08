fb7855171604c37023645432ded2d9fc2ba9ebcf
Sierraki <Sierrakiper@outlook.com>
2025-05-08 10:33:35 +0800

    20250508 1033

--- 草稿.py
+++ 草稿.py
@@ -1,14 +1,17 @@
-s = "aabccabba"
+s = "cabaabac"
 
+s = [i for i in s]
 n = len(s)
 left, right = 0, n - 1
-while left <= right:
+
+while left < right:
     while s[left] == s[right]:
-        if s[left] == s[left + 1]:
-            left += 1
-        if s[right] == s[right - 1]:
-            right -= 1
-    while s[left+1] == s[right-1]:
-        left+=1
-        right-=1
-    print(left, right)
+        right -= 1
+    while s[left] == s[left + 1]:
+        left += 1
+    if s[left] != s[left + 1]:
+        left += 1
+
+    if s[left] != s[right]:
+        break
+print(right - left + 1)
