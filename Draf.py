55f95a0da2b81095f28725d96204ef337acf95fc
Sierraki <Sierrakiper@oulook.com>
2025-05-12 08:46:41 +0800

    20250512 0846

--- Draf.py
+++ Draf.py
@@ -1,10 +1,10 @@
-s = "abc"
-t = "bac"
+tickets = [2, 3, 2]
+k = 2
 
-cur = 0
-for idx, i in enumerate(s):
-
-    lc = t.index(i)
-    print(idx, lc)
-    cur += abs(idx - lc)
-print(cur)
+cnt = 0
+while tickets[k] > 0:
+    for i in range(len(tickets)):
+        if tickets[i]>0:
+            tickets[i]-=1
+            cnt+=1
+print(cnt)
