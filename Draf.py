20f27a4a7f59eb0332784a06989827f0f09cfb79
Sierraki <Sierrakiper@outlook.com>
2025-05-13 22:18:17 +0800

    20250513 2155

--- Draf.py
+++ Draf.py
@@ -1,20 +1,7 @@
-from collections import Counter
+num = 1800
 
-num1 = 1
-num2 = 10
-num3 = 100
 
-nums = [str(num1), str(num2), str(num3)]
-for i in range(len(nums)):
-    nums[i] = nums[i].zfill(4)
-print(nums)
-a = []
-for i in range(len(nums[0])):
-    mi = 9
-    for j in nums:
-        mi = min(mi, int(j[i]))
-    a.append(mi)
+nums = str(num)
+print(nums[-1])
 
-print(a)
-b = "".join(map(str, a))
-print(int(b))
+if nums[-1]==0
