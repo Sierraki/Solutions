a27e7a81d60ac17dcec30d6104e634fb32695f7f
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-14 22:38:31 +0800

    20250514  2238

--- Draf.py
+++ Draf.py
@@ -1,6 +1,13 @@
 import bisect
 from collections import Counter, defaultdict
+from itertools import count
+from datetime import datetime
 
-
-a = ["" ]* 10
-print(a)
+n = 11
+a = bin(n)[2:]
+for i in range(len(a) - 1):
+    if a[i] == a[i + 1]:
+        print(False)
+        break
+else:
+    print(True)
