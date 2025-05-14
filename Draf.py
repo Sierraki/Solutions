de79b261c957ac47c006d914e5c372faee7a2d87
Sierraki <Sierrakiper@oulook.com>
2025-05-14 11:23:32 +0800

    20250514 1100

--- Draf.py
+++ Draf.py
@@ -1,10 +1,2 @@
-tickets = [2, 3, 2]
-k = 2
+cnt=2
 
-cnt = 0
-while tickets[k] > 0:
-    for i in range(len(tickets)):
-        if tickets[i]>0:
-            tickets[i]-=1
-            cnt+=1
-print(cnt)
