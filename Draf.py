d57d06c7e1361fd4c3787d52845bf8df3b56eb16
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-15 11:18:50 +0800

    20250515 1118

--- Draf.py
+++ Draf.py
@@ -3,11 +3,3 @@ from collections import Counter, defaultdict
 from itertools import count
 from datetime import datetime
 
-nums = [1, 3, 6, 10, 12, 15]
-
-cur = cnt = 0
-for i in nums:
-    if i % 6 == 0:
-        cur += i
-        cnt += 1
-print(cur // cnt)
