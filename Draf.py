e02e9a7daa58fade84bfa2d005aa0ce99370c74c
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-17 00:00:54 +0800

    20250516 2300

--- Draf.py
+++ Draf.py
@@ -1,37 +1,18 @@
 from collections import Counter, defaultdict
+import bisect
 
+nums = [4,5,6,7,0,1,2]
+target = 0
 
-word = "aeiou"
-k = 0
+left,right=0,len(nums)-1
 
-cnt = {"a": 0, "e": 0, "i": 0, "o": 0, "u": 0}
-fcnt = Counter()
-su = 0  # total cnt
-
-for i in word[: (5 + k)]:
-    if i in cnt:
-        cnt[i] += 1
+while left<right:
+    mid=(right+left)//2
+    if nums[mid]>nums[right]:
+        left=mid+1
+    elif nums[mid]<nums[right]:
+        right=mid
     else:
-        fcnt[i] += 1
-if len(cnt) >= 5 and len(fcnt) == k and min(fcnt.values()) == 1 :
-    su += 1
-wl = k + 5
-for i in range(k + 5, len(word)):
-    # remove
-    if word[i - wl] in cnt:
-        cnt[word[i - wl]] -= 1
-    else:
-        fcnt[word[i - wl]] -= 1
-        if fcnt[word[i - wl]] == 0:
-            del fcnt[word[i - wl]]
-
-    # add
-    if word[i] in cnt:
-        cnt[word[i]] += 1
-    else:
-        fcnt[word[i]] += 1
-
-    if min(cnt.values()) >= 1 and len(fcnt) == k and min(fcnt.values()) == 1:
-        su += 1
-    print(word[i - wl + 1 : i + 1], su, cnt, fcnt)
-print(su)
+        right-=1
+print(left)
+if n
