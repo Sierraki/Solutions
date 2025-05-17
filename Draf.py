80f385fe5d74bb24f28ec80be968cea04be39ff5
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-17 21:34:04 +0800

    20250517 1445

--- Draf.py
+++ Draf.py
@@ -1,18 +1,20 @@
-from collections import Counter, defaultdict
-import bisect
+from typing import List
+from math import sqrt, floor
 
-nums = [4,5,6,7,0,1,2]
-target = 0
-
-left,right=0,len(nums)-1
-
-while left<right:
-    mid=(right+left)//2
-    if nums[mid]>nums[right]:
-        left=mid+1
-    elif nums[mid]<nums[right]:
-        right=mid
-    else:
-        right-=1
-print(left)
-if n
+m = 2
+n = 3
+indices = [[0, 1], [1, 1]]
+row = [0] * n
+print(row)
+ma = []
+for _ in range(m):
+    ma.append(row)
+print(ma)
+for i in indices:
+    # row
+    for j in range(n):
+        ma[i[0]][j] += 1
+    # col
+    for k in range(m):
+        ma[k][i[1]] += 1
+    print(ma)
