98544353e719970e3ab21ba2347ae19c1780b188
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-18 14:58:47 +0800

    20250518 1458

--- Draf.py
+++ Draf.py
@@ -3,38 +3,22 @@ from bisect import bisect
 from typing import List
 from math import sqrt, floor
 
+mat = [[1, 0, 0], [0, 1, 0], [0, 0, 1]]
+col = []
+for i in range(len(mat[0])):
+    a = []
+    for j in mat:
+        a.append(j[i])
+    col.append(a)
+print(mat)
+print(col)
 
-source = [1, 2, 3, 4, 5]
-target = [3, 4, 5, 1, 2]
-
-# 对source遍历
-# 同个位置上不同的打tag
-# 建立source字典，val：idx
-
-map1 = {i: idx for idx, i in enumerate(source)}
-print(map1)
-check = [False] * len(source)
-tcnt = 0
-print(source)
-print(target)
-for idx, i in enumerate(source):
-    cnt = 0
-    if not check[idx]:
-        # 指针指向当前位置，检测当前位置是不是true
-        # 是true跳过，不是就继续
-        # 不是的话，给指针位置打tag
-        # 找出要换的数字在source中的位置lc
-        # 对换当前lc和pin位置，pin挪到lc，继续
-        pin = idx
-        while (
-            check[pin] == False
-            and source[map1[target[pin]]] != target[map1[target[pin]]]
-        ):
-            check[pin] = True
-            lc = map1[target[pin]]
-
-            source[pin], source[lc] = source[lc], source[pin]
-            pin = lc
-            cnt += 1
-        tcnt += cnt
-print(tcnt)
+rows = [sum(i) for i in mat]
+cols = [sum(i) for i in col]
+cnt = 0
+for i in range(len(rows)):
+    if rows[i] + cols[i] == 1:
+        cnt += 1
+print(rows)
+print(cols)
+print(cnt)
