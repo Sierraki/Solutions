96f69ee995546c23dddd7579e185de17007c04bf
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-19 12:56:57 +0800

    20250519 1256

--- Draf.py
+++ Draf.py
@@ -3,14 +3,3 @@ from bisect import bisect
 from typing import List
 from math import sqrt, floor
 
-nums = [2, 7, 11, 15]
-target = 9
-
-a = {}
-for idx, i in enumerate(nums):
-    goal = target - i
-    if goal not in a:
-        a[i] = idx
-    else:
-        print(a[goal], idx)
-        break
