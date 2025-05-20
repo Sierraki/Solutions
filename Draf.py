47b0c2eef5704b07163ae2535c4366dc0f83e424
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-20 15:13:52 +0800

    20250520 1513

--- Draf.py
+++ Draf.py
@@ -4,3 +4,20 @@ from typing import List
 from math import sqrt, floor
 
 
+nums = [1, 2, 3, 3, 2, 2]
+
+target = max(nums)
+
+mx = cur = 0
+left = right = 0
+
+while right < len(nums):
+    if nums[right] == nums[left] == target:
+        right += 1
+        cur += 1
+        mx = max(cur, mx)
+    else:
+        right += 1
+        left = right
+        cur = 0
+print(mx)
