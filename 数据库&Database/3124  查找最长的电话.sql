3176f781688838a49e94a81b692e9316c7690a84
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-22 00:16:28 +0800

    main

--- 数据库&Database/3124  查找最长的电话.sql
+++ 数据库&Database/3124  查找最长的电话.sql
@@ -1,30 +1,35 @@
+WITH a1 AS
+(
+	SELECT  first_name
+	       ,type
+	       ,duration
+	FROM calls
+	LEFT JOIN contacts
+	ON id = contact_id
+) , a2 AS
+(
+	SELECT  *
+	FROM a1
+	WHERE type = 'incoming'
+	ORDER BY duration DESC
+	LIMIT 3
+) , a3 AS
+(
+	SELECT  *
+	FROM a1
+	WHERE type = 'outgoing'
+	ORDER BY duration DESC
+	LIMIT 3
+) , a4 AS
+(
+	SELECT  *
+	FROM a2
+	UNION
+	SELECT  *
+	FROM a3
+)
 SELECT  first_name
        ,type
-       ,concat( lpad(floor(duration/3600),2,0),':',lpad(floor((duration-floor(duration/3600)*3600)/60),2,0),':',lpad(duration%60,2,0) ) AS duration_formatted
-FROM (
-    SELECT  first_name
-           ,type
-           ,duration
-    FROM calls
-    LEFT JOIN contacts
-    ON id = contact_id
-    WHERE type = 'incoming'
-    ORDER BY duration DESC
-    LIMIT 3
-) a2
-UNION
-SELECT  first_name
-       ,type
-       ,duration
-FROM (
-    SELECT  first_name
-           ,type
-           ,duration
-    FROM calls
-    LEFT JOIN contacts
-    ON id = contact_id
-    WHERE type = 'outgoing'
-    ORDER BY duration DESC
-    LIMIT 3
-) a3
-ORDER BY type DESC, duration DESC, first_name DESC;
+       ,concat( lpad(floor(duration/3600),2,0),':',lpad(floor((duration-floor(duration/3600)*3600)/60),2,0),':',lpad(duration%60,2,0) )duration_formatted
+FROM a4
+ORDER BY type desc, duration desc, first_name desc
