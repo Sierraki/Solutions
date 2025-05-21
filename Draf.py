b2b8ab169ae6bc7dad1f461db51f9d6efd191521
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-21 17:54:34 +0800

    20250521 1754

--- Draf.py
+++ Draf.py
@@ -1,21 +1,2 @@
-from collections import defaultdict, Counter
-import bisect
-from typing import List
-from math import sqrt, floor
-
-
-nums = [1, 2, -1, -2, 1, 0, -1]
-
-nums = list(set(nums))
-nums.sort(reverse=True)
-
-cur = nums[0]
-print(nums,cur)
-for i in range(1, len(nums)):
-    last = cur
-    cur += nums[i]
-    if cur <= last:
-
-        break
-
-print(last)
+nums = [1, 2, 3, 5, 8, 6, 1, 1, 1]
+cnt = counter(nums)
