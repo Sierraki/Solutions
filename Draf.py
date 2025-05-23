fc442fe94ca464f2f41b5145174d67a8a650d952
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-23 20:58:38 +0800

    20250523 2058

--- Draf.py
+++ Draf.py
@@ -1,14 +1,13 @@
 from collections import defaultdict, Counter
 from math import sqrt, floor
-import bisect
+import bisect, re
 
-costs = [[17, 2, 17], [16, 16, 5], [14, 3, 19]]
-n = len(costs)
 
-for i in range(1, n):
-    for j in range(3):
-        b = costs[i - 1]
-        a = b.copy()
-        del a[j]
-        costs[i][j] += min(a)
+s = "abccbaacz"
 
+cnt=Counter()
+
+for i in s:
+    cnt[i] += 1
+    if cnt[i]==2:
+        
