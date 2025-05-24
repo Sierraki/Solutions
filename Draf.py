cb43bcdf2c9c46cff02cefd0399e0bac1af63c1f
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-24 23:41:46 +0800

    20250524 2341

--- Draf.py
+++ Draf.py
@@ -2,19 +2,59 @@ from collections import defaultdict, Counter
 from math import sqrt, floor
 import bisect, re
 
+word = "abcdeafdef"
 
-s = "abcdefg"
-shift = [[1, 1], [1, 1], [0, 2], [1, 3]]
+cnt = defaultdict(list)
+for idx, i in enumerate(word):
+    cnt[i].append(idx)
 
-n = len(s)
-for i in shift:
-    a = s + s
-    if i[0] == 0:
-        a = s + s
-        t = i[1] % n
-        s = a[t : t + n]
-    else:
-        t = i[1] % n
-        s = a[n - t : 2 * n - t]
-    print(s)
-print(s)
+print(dict(cnt))
+
+nums = [
+    (2, 7),
+    (2, 8),
+    (2, 16),
+    (2, 17),
+    (3, 9),
+    (3, 19),
+    (3, 21),
+    (4, 10),
+    (4, 11),
+    (4, 13),
+    (4, 15),
+    (4, 20),
+    (5, 9),
+    (5, 19),
+    (5, 21),
+    (6, 10),
+    (6, 11),
+    (6, 13),
+    (6, 15),
+    (6, 20),
+    (7, 16),
+    (7, 17),
+    (8, 16),
+    (8, 17),
+    (9, 19),
+    (9, 21),
+    (10, 13),
+    (10, 15),
+    (10, 20),
+    (11, 15),(15, 20),
+    (11, 20),
+    (12, 18),
+    (13, 20),
+    (14, 18),
+    
+]
+nums.sort(key=lambda x: x[1])
+
+cnt = 0
+le = -1
+
+for start, end in nums:
+    if start > le:
+        cnt += 1
+        le = end
+
+print(cnt)
