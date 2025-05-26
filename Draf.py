0d15b7d36ec691e451722e38b7202453e797e92d
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-26 13:50:47 +0800

    refactor(Draf.py): 重构代码并添加 CustomStack 类
    - 删除了未使用的导入和变量
    - 移除了原有的字符串比较逻辑
    - 添加了 CustomStack 类，实现了一个自定义栈结构
    - 优化了代码结构，提高了代码的可读性和可维护性

--- Draf.py
+++ Draf.py
@@ -3,21 +3,20 @@ from math import sqrt, floor
 import bisect, re
 from functools import lru_cache
 
-s1 = "attack"
-s2 = "defend"
 
-cnt1 = Counter(s1)
-cnt2 = Counter(s2)
-if cnt1 != cnt2:
-    print(False)
-else:
-    cnt = defaultdict(int)
-    n = len(s1)
-    for i in range(n):
-        if s1[i] != s2[i]:
-            res = tuple(sorted([s1[i], s2[i]]))
-            cnt[res] += 1
-    if len(cnt) > 1 or max(cnt.values()) > 2:
-        print(False)
-    else:
-        print(True)
+class CustomStack:
+
+    def __init__(self, maxSize: int):
+        self.stack = []
+        self.n = 0
+        self.maxSize=maxSize
+
+    def push(self, x: int) -> None:
+        if self.n < self.maxSize:
+            self.stack.append(x)
+            self.n += 1
+
+    def pop(self) -> int:
+        if n > 0:
+            stack.pop()
+            n -= 1
