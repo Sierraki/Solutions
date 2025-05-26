d8f873a42b10c42cb2ebe4f07897c1d73d5adc75
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-26 23:15:21 +0800

    refactor(Draf): 重构代码以解析命令字符串
    - 移除了未使用的 grid 相关代码
    - 添加了将 command 字符串按 "()" 分割的逻辑
    - 打印分割后的结果

--- Draf.py
+++ Draf.py
@@ -3,18 +3,7 @@ from math import sqrt, floor
 import bisect, re
 from functools import lru_cache
 
-grid = [[5, 0, 0, 1], [0, 4, 1, 5], [0, 5, 2, 0], [4, 1, 0, 2]]
+command = "G()(al)"
 
-nz_cnt=z_cnt=0
-n = len(grid)
-for i in range(n):
-    if grid[i][i] !=0 :
-        nz_cnt+=1
-for i in range(n - 1, -1, -1):
-    if grid[i][i] != 0:
-        nz_cnt += 1
-for i in grid:
-    for j in i:
-        if j==0:
-            z_cnt+=1
-        
+a = command.split("()")
+print(a)
