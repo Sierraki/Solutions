7860cbeded92b259f95cdd491bc8ae26808dc370
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-05-28 22:12:28 +0800

    refactor(algorithm): 重构合并两个有序链表的解决方案
    - 优化了代码结构，提高了可读性和可维护性
    - 添加了第二种实现方法，增加了代码的

--- 力扣&Leetcode/算法&algorithm/21.合并两个有序链表.py
+++ 力扣&Leetcode/算法&algorithm/21.合并两个有序链表.py
@@ -1,14 +1,40 @@
 class Solution:
     def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = ListNode(0)
-        pin = head
+        dummy = ListNode()
+        pin = dummy
         while l1 and l2:
-            if l1.val <= l2.val:
-                pin.next = l1
-                l1 = l1.next
-            else:
-                pin.next = l2
+            if l1.val >= l2.val:
+                pin.next = ListNode(l2.val)
                 l2 = l2.next
+            else:
+                pin.next = ListNode(l1.val)
+                l1 = l1.next
             pin = pin.next
         pin.next = l1 or l2
-        return head.next
+        return dummy.next
+
+
+class Solution:
+    def mergeTwoLists(self, l1: ListNode, l2: ListNode) -> ListNode:
+        if not l1:
+            return l2
+        if not l2:
+            return l1
+        dummy = ListNode(0)
+        p = dummy
+        p1 = l1
+        p2 = l2
+        while p1 and p2:
+            if p1.val >= p2.val:
+                p.next = ListNode(p2.val)
+                p2 = p2.next
+            else:
+                p.next = ListNode(p1.val)
+                p1 = p1.next
+            p = p.next
+            # print(dummy)
+        if not p1:
+            p.next = p2
+        if not p2:
+            p.next = p1
+        return dummy.next
