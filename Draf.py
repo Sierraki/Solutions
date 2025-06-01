68e987e7aec50a76126340c8ebbef533bfdf81ba
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-06-01 22:34:06 +0800

    refactor(Draf.py): 重构代码并添加 logging 配置
    - 移除了 grid 和 k 的定义，替换成 nums 和 target
    - 删除了未使用的变量和代码块
    - 添加了 logging 配置用于调试
    - 简化了检查等式分区的逻辑

--- Draf.py
+++ Draf.py
@@ -2,30 +2,24 @@ from collections import defaultdict, Counter
 from math import sqrt, floor
 import bisect, re
 from functools import lru_cache
+import logging
 
-grid = [[1, -2, 3], [2, 3, 5]]
-k = 2
+# 配置 logging
+logging.basicConfig(
+    level=logging.DEBUG,  # 设置日志级别
+)
 
-m, n = len(grid), len(grid[0])
-ans = []
 
-for i in range(m - k + 1):
-    row = []
-    for j in range(n - k + 1):
-        val = set()
-        for x in range(i, i + k):
-            for y in range(j, j + k):
-                val.add(grid[x][y])
-        if len(val) == 1:
-            row.append(0)
-        else:
-            nums = sorted(val)
-            mi = float("inf")
-            for a in range(1, len(nums)):
-                diff = nums[a] - nums[a - 1]
-                if diff < mi:
-                    mi = diff
-            row.append(mi)
-        print(mi)
-    ans.append(row)
-print(ans)
+nums = [1, 2, 8]
+target = 4
+
+sum = 1
+for i, x in enumerate(nums):
+    if (target % x):
+        return False
+    sum *= x
+    
+if sum / target == target:
+    return True
+    
+return False      
--- 力扣&Leetcode/算法&algorithm/周赛/452周赛/Q1.等积子集的划分方案.py
+++ 力扣&Leetcode/算法&algorithm/周赛/452周赛/Q1.等积子集的划分方案.py
@@ -1,28 +1,10 @@
 class Solution:
     def checkEqualPartitions(self, nums: List[int], target: int) -> bool:
-        def backtrack(start, product, path):
-            if product == target and path:
-                return path
-            for i in range(start, len(nums)):
-                num = nums[i]
-                if product != 0 and abs(product * num) > abs(target):
-                    continue
-        
-                res = backtrack(i + 1, product * num, path + [num])
-                if res is not None:
-                    return res
-            return None
-    
-        
-        ans = backtrack(0, 1, [])
-        if ans==None:
-            return False
-        a = [x for x in nums if x not in ans]
-        cnt1 = 1
-        cnt2 = 1
-        for i in ans:
-            cnt1 *= i
-        for i in a:
-            cnt2 *= i
-        print(cnt1 == cnt2)
-        return (cnt1 == cnt2)
+        s = 1
+        for i, x in enumerate(nums):
+            if (target % x) != 0:
+                return False
+            s *= x
+        if s / target == target:
+            return True
+        return False
