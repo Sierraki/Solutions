a4ab150fe70c5cc6c5d06d602e5c822818bbb857
Sierraki <Sierrakiper@outlook.com>
2025-06-19 03:52:08 +0800

    20250619 0352

--- Draf.py
+++ Draf.py
@@ -4,5 +4,15 @@ import bisect, re
 from functools import lru_cache
 from collections import deque
 
-coins = [1, 2, 5]
+numbers = [1, 2, 4, 6, 10]
+target = 8
 
+ans = -1
+check = Counter()
+for idx, i in enumerate(numbers):
+    tar = target - i
+    if tar not in check:
+        check[tar] += idx
+    if i in check:
+        ans = [check[i], idx]
+print(ans)
