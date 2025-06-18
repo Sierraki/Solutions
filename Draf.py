3655c0120352dfef920b92df2ba92bf9ee58e26b
Sierraki <outlook.com>
2025-06-19 02:08:42 +0800

    123

--- Draf.py
+++ Draf.py
@@ -5,15 +5,3 @@ from functools import lru_cache
 from collections import deque
 
 coins = [1, 2, 5]
-amount = 11
-
-if amount < 1:
-    return 0
-dp = [float("inf")] * (amount + 1)
-dp[0] = 0
-for i in range(1, amount + 1):
-    for j in coins:
-        if j <= i:
-            dp[i] = min(dp[i], 1 + dp[i - j])
-return -1 if dp[amount] == float("inf") else dp[amount]
-
