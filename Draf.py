a26451a667d6e4919fbd48ed54ca56876d0c56e8
Sierraki <Sierrakiper@outlook.com>
2025-06-25 02:56:07 +0800

    20250624 1530

--- Draf.py
+++ Draf.py
@@ -4,33 +4,50 @@ import bisect, re
 from functools import lru_cache
 from collections import deque
 
-s = "aaaabbbbcccc"
-cnt = Counter(s)
-res = [[i, j] for i, j in cnt.items()]
-res.sort()
-print(res)
-ans = []
-while res:
-    # ---->
-    l = 0
-    while l < len(res):
-        if res[l][1] > 0:
-            ans.append(res[l][0])
-            res[l][1] -= 1
-            if res[l][1] == 0:
-                del res[l]
-            else:
-                l += 1
+candies = 600
+num_people = 40
 
-    # <----
-    r = -1
-    while r >= -len(res):
-        if res[r][1] > 0:
-            ans.append(res[r][0])
-            res[r][1] -= 1
-            if res[r][1] == 0:
-                del res[r]
-            else:
-                r -= 1
 
-print("".join(ans))
+times = int((-1 + (1 + 8 * candies) ** 0.5) // 2)
+res = [0] * (num_people)
+a = 0
+if times <= num_people:
+    for i in range(len(res)):
+        if candies >= i + 1:
+            res[i] = i + 1
+            candies -= i + 1
+        elif candies > i + 1:
+            res[i] = candies
+            a += 1
+
+    print(res)
+else:
+    # fib阶数
+    tim = int((-1 + (1 + 8 * candies) ** 0.5) // 2)
+    # 循环次数
+    c = tim // num_people
+    res = [i for i in range(1, num_people + 1)]
+    if c > 1:
+        for i in range(len(res)):
+            res[i] += (c - 1) * num_people
+    cnt = [0] * len(res)
+    for idx, i in enumerate(res, start=1):
+        cnt[idx - 1] = (i + idx) * c // 2
+
+    res = cnt
+    # 循环后剩下的糖果
+    left = candies - sum(res)
+    print(left)
+    # 开始计数的数量
+    start = c * num_people + 1
+    print(start)
+
+    for idx, i in enumerate(res):
+        if left >= start:
+            res[idx] += start
+            left -= start
+            start += 1
+        else:
+            res[idx] += left
+            print(res)
+            break
