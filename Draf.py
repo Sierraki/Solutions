c27cb9534949c9252fd0078659058f78c8317cf1
Sierraki <Sierrakiper@outlook.com>
2025-07-03 04:39:43 +0800

    20250703 0439

--- Draf.py
+++ Draf.py
@@ -4,27 +4,8 @@ import bisect, re
 from functools import lru_cache
 from collections import deque
 
-m = 18
-n = 3
-ops = [
-    [16, 1],
-    [14, 3],
-    [14, 2],
-    [4, 1],
-    [10, 1],
-    [11, 1],
-    [8, 3],
-    [16, 2],
-    [13, 1],
-    [8, 3],
-    [2, 2],
-    [9, 1],
-    [3, 1],
-    [2, 2],
-    [6, 3],
-]
 
-res1 = [i for i, j in ops]
-res2 = [j for i, j in ops]
+a = "*"
 
-print(min(res1) * min(res2))
+for i in range(2, -1, -1):
+    print(" " * (i) + a * (5 - i * 2) + " " * (i))
---
+++ 洛谷&Luogu/B2025 输出字符菱形.py
@@ -0,0 +1,4 @@
+for i in range(2, -1, -1):
+    print(" " * i + "*" * (5 - 2 * i) + " " * i)
+for i in range(1, 3):
+    print(" " * i + "*" * (5 - 2 * i) + " " * i)
