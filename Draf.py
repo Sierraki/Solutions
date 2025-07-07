ebc64db8cdc9e15512b987b488e413c15b348f58
Sierraki <Sierrakiper@outlook.com>
2025-07-08 04:19:03 +0800

    20250708 0330

--- Draf.py
+++ Draf.py
@@ -6,16 +6,12 @@ from collections import deque
 from typing import List
 
 
-nums = [7, 8, 3, 4, 15, 13, 4, 1]
+words = ["pay", "attention", "practice", "attend"]
+pref = "at"
 
-nums.sort()
-n = len(nums)
-print(nums)
-res = float('inf')
-
-for _ in range(n // 2):
-    ans = (nums[0] + nums[-1]) / 2
-    del nums[0]
-    nums.pop()
-    res=min(res,ans)
-print(res)
+ans = 0
+for i in words:
+    if len(i) >= len(pref):
+        if i[: len(pref)] == pref:
+            ans += 1
+print(ans)
