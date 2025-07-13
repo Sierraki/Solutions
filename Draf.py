87be5aa41753e043b6012a4b88f533a603ce1798
Sierraki <Sierrakiper@outlook.com>
2025-07-14 03:58:41 +0800

    20250714 0340

--- Draf.py
+++ Draf.py
@@ -5,22 +5,16 @@ from functools import lru_cache
 from collections import deque
 from typing import List
 
+
 text1 = "abcde"
 text2 = "ace"
-
-
 m = len(text1)
 n = len(text2)
-
 dp = [[0] * (m + 1) for _ in range(n + 1)]
-
-print(dp)
-
 for i in range(1, n + 1):
     for j in range(1, m + 1):
-        if text1[j-1] == text2[i-1]:
+        if text1[j - 1] == text2[i - 1]:
             dp[i][j] = dp[i - 1][j - 1] + 1
         else:
             dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
-
 print(dp)
