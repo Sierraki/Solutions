d24da2803814f5bf00fd4d150adabfce1090e0d7
Sierraki <Sierrakiper@outlook.com>
2025-07-16 17:59:12 +0800

    20250716 1530

--- Draf.py
+++ Draf.py
@@ -5,25 +5,20 @@ from collections import deque
 from typing import List
 
 
-events = [
-    [7, 1],
-    [19, 3],
-    [9, 4],
-    [12, 5],
-    [2, 8],
-    [15, 10],
-    [18, 12],
-    [7, 14],
-    [19, 16],
-]
+grid = [[3, 2], [1, 3], [3, 4], [0, 1]]
 
-cnt = Counter()
+n = len(grid)
+m = len(grid[0])
 
-for idx, i in enumerate(events):
-    if idx == 0:
-        cnt[i[0]] += i[1]
-    elif idx > 0:
-        cnt[i[0]] += i[1] - events[idx - 1][1]
-tar = max(cnt.values())
-res = [i for i, j in cnt.items() if j == tar]
-print(cnt)
+res = [[0] * n for _ in range(m)]
+
+for i in range(m):
+    for j in range(n):
+        res[i][j] = grid[j][i]
+ans = 0
+for i in res:
+    for j in range(1, n):
+        if i[j] <= i[j - 1]:
+            ans += i[j - 1] - i[j] + 1
+            i[j] = i[j - 1] + 1
+print(res, ans)
