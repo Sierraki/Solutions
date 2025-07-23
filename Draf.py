ec741bad110fa726ab6c3774e3d8277ff5acfe2a
Sierraki <Sierrakiper@outlook.com>
2025-07-23 23:41:03 +0800

    20250723 2341

--- Draf.py
+++ Draf.py
@@ -4,19 +4,3 @@ import bisect, re
 from collections import deque
 from typing import List
 
-word = "acolkxjbizfmhnrdq"
-
-n = len(word)
-
-a = n // 8
-b = n % 8
-
-res = 0
-if a == 0:
-    res = b
-elif a == 1:
-    res = 8 + b * 2
-else:
-    res = (1 + a) * a * 4 + b * (a + 1)
-
-print(res)
