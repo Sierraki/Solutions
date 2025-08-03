bcd4658d4ecefd6e48090ba4d849cab4ff9d1dbf
Sierraki <Sierrakiper@outlook.com>
2025-08-04 04:20:00 +0800

    20250804 0419

--- Draf.py
+++ Draf.py
@@ -4,22 +4,71 @@ import bisect, re
 from collections import deque
 from typing import List
 
-s = "10#11#12"
+
+date1 = "2019-06-29"
+date2 = "2019-06-30"
+monp = {
+    1: 0,
+    2: 31,
+    3: 59,
+    4: 90,
+    5: 120,
+    6: 151,
+    7: 181,
+    8: 212,
+    9: 243,
+    10: 273,
+    11: 304,
+    12: 334,
+}
+monr = {
+    1: 0,
+    2: 31,
+    3: 60,
+    4: 91,
+    5: 121,
+    6: 152,
+    7: 182,
+    8: 213,
+    9: 244,
+    10: 274,
+    11: 305,
+    12: 335,
+}
 
 
-pin = len(s) - 1
-cnt = {i - 96: chr(i) for i in range(ord("a"), ord("z") + 1)}
-res = ""
+def fun(s: str) -> int:
+    y = int(s[:4])
+    m = int(s[5:7])
+    d = int(s[8:])
 
-while pin >= 0:
-    if s[pin].isdigit():
-        res += cnt[int(s[pin])]
-        pin -= 1
-    elif s[pin] == "#":
-        res += cnt[int(s[pin - 2 : pin])]
-        pin -= 3
+    swap = False
+    if y % 4 == 0 and y % 100 != 0 or (y % 400 == 0):
+        swap = True
+
+    res = 0
+    if y <= 1972:
+        if y == 1971:
+            res = 0
+            res += monr[m] + d
+        else:
+            res = 365
+            res += monp[m] + d
+    else:
+        dif = y - 1972
+        res = dif * 365 + 365 + 366 + dif // 4
+        if swap:
+            res -= 366
+            res += monr[m] + d
+        else:
+            res -= 365
+            res += monp[m] + d
+    return res
 
 
-# ans = "".join(res)
-print(res[::-1])
-# print(ans)
+print(abs(fun(date1) - fun(date2)))
+
+
+print(fun("2100-09-22"))
+
+print(fun("1991-03-12"))
