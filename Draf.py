649f83bf2bc75adcb0f82e96fe068eecfd9e95c0
Sierraki <Sierrakiper@outlook.com>
2025-08-09 22:41:08 +0800

    20250809 2241

--- Draf.py
+++ Draf.py
@@ -6,26 +6,3 @@ from typing import List
 from fractions import Fraction
 
 
-class TripleInOne:
-
-    def __init__(self, stackSize: int):
-        self.nums = deque([])
-        self.n = stackSize
-
-    def push(self, stackNum: int, value: int) -> None:
-        if len(self.nums) < self.n:
-            self.nums.insert(stackNum, value)
-        return self.nums
-
-    def pop(self, stackNum: int) -> int:
-        if not self.nums:
-            return -1
-        del self.nums[stackNum]
-
-    def peek(self, stackNum: int) -> int:
-        if not self.nums:
-            return -1
-        return self.nums[stackNum]
-
-    def isEmpty(self, stackNum: int) -> bool:
-        
