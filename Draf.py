1bb3d1203578d498097b01908175a335fd37163e
Sierraki <Sierrakiper@outlook.com>
2025-08-13 02:48:05 +0800

    20250813 0248

--- Draf.py
+++ Draf.py
@@ -6,44 +6,16 @@ from typing import List, Optional
 from fractions import Fraction
 
 
-class Solution:
-    def increasingBST(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
-        """
-        将一个二叉搜索树（BST）转换为一个右倾斜的、递增顺序的搜索树。
-        该方法使用中序遍历，并直接在遍历过程中重新链接节点。
+def fun(ans=list, x=int, y=int) -> bool:
+    for idx, i in enumerate(ans):
+        if x in i and y in i and idx > 1:
+            for j in range(1, len(i), 2):
+                if (i[j] == x and i[j - 1] == y) or (i[j] == y and i[j - 1] == x):
+                    return False
+            else:
+                return True
+    return False
 
-        Args:
-            root (Optional[TreeNode]): 输入的二叉搜索树的根节点。
 
-        Returns:
-            Optional[TreeNode]: 转换后树的根节点。
-        """
-        # 使用一个虚拟头节点来简化代码，它将指向新树的根。
-        dummy = TreeNode()
-        # pre 指针用于追踪中序遍历中的上一个节点，初始指向虚拟头节点。
-        pre = dummy
-
-        def inorder(node: Optional[TreeNode]):
-            nonlocal pre
-            if not node:
-                return
-
-            # 1. 递归遍历左子树。
-            inorder(node.left)
-
-            # 2. 处理当前节点（中序遍历的核心）。
-            # 移除当前节点的左子节点，因为新树中没有左子节点。
-            node.left = None
-            # 将上一个节点的右子节点指向当前节点。
-            pre.right = node
-            # 更新 pre 指针，使其指向当前节点，为下一个节点做准备。
-            pre = node
-
-            # 3. 递归遍历右子树。
-            inorder(node.right)
-
-        # 开始中序遍历
-        inorder(root)
-
-        # 返回虚拟头节点的右子节点，它就是新树的根。
-        return dummy.right
+print(fun([[1], [2, None, 3, None, None], [4, None, None]], 4, 5))
+print(fun([[1], [2, None, 3, None, None], [4, None, None]], 2, 3))
--- 力扣&Leetcode/算法&algorithm/方法/二叉树/BFS列表转二叉树.py
+++ 力扣&Leetcode/算法&algorithm/方法/二叉树/BFS列表转二叉树.py
@@ -18,3 +18,36 @@ def build_tree_from_list(nodes: List[Optional[int]]) -> Optional[TreeNode]:
             res.append(cur.right)
         pin += 1
     return root
+
+
+# 加入None
+def level_order_with_none(root):
+    if not root:
+        return []
+
+    queue = deque([root])
+    result = []
+
+    while queue:
+        level_list = []
+        # 标记这一层是否全是 None，用于后续优化
+        is_all_none = True
+
+        for _ in range(len(queue)):
+            node = queue.popleft()
+
+            if node:
+                level_list.append(node.val)
+                is_all_none = False
+                # 无论子节点是否存在，都将其添加到队列
+                queue.append(node.left)
+                queue.append(node.right)
+            else:
+                level_list.append(None)
+                # None 节点没有子节点，所以这里不需要再添加到队列
+
+        # 如果这一层不是全 None，才添加到结果中
+        if not is_all_none:
+            result.append(level_list)
+
+    return result
