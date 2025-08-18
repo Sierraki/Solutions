5d93a3175d6fe7dc8766a7a5320d20e4f415ddfd
Sierraki <Sierrakiper@outlook.com>
2025-08-19 03:15:22 +0800

    20250819 0315

--- Draf.py
+++ Draf.py
@@ -6,20 +6,3 @@ from collections import deque
 from typing import List, Optional
 from fractions import Fraction
 
-nums = [[1, 2, 5], [1, 3]]
-
-
-def fun1(nums):
-    if len(nums) > 1:
-        for i in range(len(nums)):
-            if i < len(nums) - 1:
-                nums[i] = str(nums[i]) + "->"
-            else:
-                nums[i] = str(nums[i])
-    else:
-        return str(nums[0])
-    return "".join(nums)
-
-
-for i in range(len(nums)):
-    print(fun1(i))
