243ff1346b4e0f494c9b6d4c74f26cad05c363e8
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-08-22 18:03:36 +0800

    20250822 1803

--- 力扣&Leetcode/算法&algorithm/70.爬楼梯.py
+++ 力扣&Leetcode/算法&algorithm/70.爬楼梯.py
@@ -10,3 +10,24 @@ class Solution:
             b = c
             i += 1
         return c
+
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        dp = [0] * (n + 1)
+        dp[0] = 1
+        dp[1] = 1
+        for i in range(2, n + 1):
+            dp[i] = dp[i - 1] + dp[i - 2]
+        return dp[n]
+
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n == 1:
+            return 1
+        a, b = 1, 1  # dp[i-2], dp[i-1]
+        for _ in range(2, n + 1):
+            a, b = b, a + b  # 更新为新的 dp[i]
+        return b
