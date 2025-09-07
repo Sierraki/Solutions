93addac0863600c68a2f06a971795f5c06d99147
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-09-07 23:48:54 +0800

    20250907 2348

--- Draf.py
+++ Draf.py
@@ -5,49 +5,17 @@ from collections import deque
 from typing import List, Optional
 from fractions import Fraction
 import pandas as pd
-
-
 import math
 
-nums = [2, 5, 3, 1, 4]
+
+n = 1
 
 
-def fun1(nums):
-    n = len(nums)
-    K = int(math.log2(n)) + 1
-    st = [[0] * K for _ in range(n)]
-    for i in range(n):
-        st[i][0] = nums[i]
-    j = 1
-    while (1 << j) <= n:
-        i = 0
-        while i + (1 << j) <= n:
-            st[i][j] = max(st[i][j - 1], st[i + (1 << (j - 1))][j - 1])
-            i += 1
-        j += 1
-    return st
+res1 = [i for i in range(1, n // 2 + 1)]
+res2 = [-i for i in range(1, n // 2 + 1)]
+if n % 2 == 1:
+    ans = res1 + res2 + [0]
+else:
+    ans = res1 + res2
 
-
-def fun2(st, L, R):
-    if L > R:
-        return -(10**18)
-    j = int(math.log2(R - L + 1))
-    return max(st[L][j], st[R - (1 << j) + 1][j])
-
-
-n = len(nums)
-if n < 3:
-    print(0)
-st = fun1(nums)
-cnt = 0
-for l in range(n - 2):
-    for r in range(l + 2, n):
-        mx = fun2(st, l + 1, r - 1)
-        if nums[l] > mx and nums[r] > mx:
-            cnt += 1
-print(cnt)
-
-
-# print(count_bowl_subarrays([2, 5, 3, 1, 4]))  # 2
-# print(count_bowl_subarrays([5, 1, 2, 3, 4]))  # 3
-# print(count_bowl_subarrays([1000000000, 999999999, 999999998]))  # 0
+print(ans)
