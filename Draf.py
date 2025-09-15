7d25f10476fcf726bf98e0b9cae60dd4cbb290a0
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-09-15 13:26:49 +0800

    update

--- Draf.py
+++ Draf.py
@@ -1,9 +1,18 @@
 from collections import defaultdict, Counter
 from math import sqrt, floor, gcd, lcm, prod, ceil
-from bisect import bisect
+from bisect import bisect, bisect_left
 from collections import deque
 from typing import List, Optional
 from fractions import Fraction
-import pandas as pd
 
+nums = [29, 25, 2, 12, 15, 42, 14, 6, 16, 9]
+n = len(nums)
+tar = 30
 
+nums.sort()
+print(nums)
+lc = bisect(nums, tar) - 1
+print(lc, nums[lc])
+
+ans = n - lc
+print(ans)
---
+++ 力扣&Leetcode/算法&algorithm/题库/239.滑动窗口的最大值.py
@@ -0,0 +1,17 @@
+class Solution:
+    def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        res = deque([])
+        ans = []
+        for i in range(len(nums)):
+            if not res:
+                res.append(nums[i])
+            else:
+                if nums[i - k] >= res[0] and i - k >= 0:
+                    res.popleft()
+                if res:
+                    while res and nums[i] > res[-1]:
+                        res.pop()
+                res.append(nums[i])
+            if i >= k - 1:
+                ans.append(res[0])
+        return ans
