a99449d37ea0490c9476564f435d9a033fe78caf
Sierraki <Sierrakiper@outlook.com>
2025-09-15 10:11:46 +0800

    update

--- Draf.py
+++ Draf.py
@@ -6,33 +6,4 @@ from typing import List, Optional
 from fractions import Fraction
 import pandas as pd
 
-turnedOn = 1
 
-# hour = 4
-# mins = 6
-
-res = []
-for i in range(5):
-    if 0 <= i <= 4 and 0 <= turnedOn - i <= 59:
-        res.append([i, turnedOn - i])
-cnt = defaultdict(list)
-
-for i in range(60):
-    cnt[bin(i)[2:].count("1")].append(str(i).zfill(2))
-
-
-def fun(a=int, b=int):
-    hour = [i for i in cnt[a] if int(i) <= 11]
-    mins = [i for i in cnt[b] if int(i) <= 59]
-    ans = []
-    for i in range(len(hour)):
-        for j in range(len(mins)):
-            ans.append(f"{int(hour[i])}:{mins[j]}")
-    return ans
-
-
-ans = []
-for i in res:
-    ans += fun(i[0], i[1])
-
-print(ans)
