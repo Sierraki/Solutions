475c586bafbbd64cbd5b960917fc2a18b715c68a
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-09-20 13:19:49 +0800

    update

--- Draf.py
+++ Draf.py
@@ -5,29 +5,6 @@ from collections import deque
 from typing import List, Optional
 from fractions import Fraction
 
-skills = [4, 18, 17, 20, 15, 12, 8, 5]
-k = 1
+res = 1.515165
 
-res = Counter()
-for idx, i in enumerate(skills):
-    res[i] = idx
-skills = deque(skills)
-cnt = Counter()
-while skills:
-    cur = skills.popleft()
-    if cur > skills[0]:
-        a = skills.popleft()
-        skills.append(a)
-        cnt[cur] += 1
-        if cnt[cur] == k:
-            ans = cur
-            break
-        skills.appendleft(cur)
-    else:
-        cnt[cur] = 0
-        cnt[skills[0]] += 1
-        if cnt[skills[0]] == k:
-            ans = skills[0]
-            break
-        skills.append(cur)
-print(res[ans])
+print(res, type(res))
---
+++ 力扣&Leetcode/算法&algorithm/方法/AMC答题格式.py
@@ -0,0 +1,8 @@
+# 一行或多行
+import sys
+for i in sys.stdin:
+    a=i.split()
+# 或者
+n=input()
+nums=input()
+
