57ac13ea28a3742dc5b3e236b1af95f2e2ac7215
Sierraki <57867019+Sierraki@users.noreply.github.com>
2025-11-09 22:38:45 +0800

    20251109

--- 力扣&Leetcode/算法&algorithm/题库/122.买卖股票的最佳时机 II.py
+++ 力扣&Leetcode/算法&algorithm/题库/122.买卖股票的最佳时机 II.py
@@ -1,17 +1,7 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        left = 0
-        cur = 0
-        n = len(prices)
-        for i in range(1, n):
-            if prices[i - 1] > prices[i]:
-                left += 1
-            else:
-                if i == n - 1:
-                    cur += prices[i] - prices[left]
-                    left = i
-                else:
-                    if prices[i + 1] < prices[i]:
-                        cur += prices[i] - prices[left]
-                        left = i
-        return cur
+        ans = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                ans += prices[i] - prices[i - 1]
+        return ans
