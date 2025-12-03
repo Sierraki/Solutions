a86328e34cb2789a8e90ed1ad0fafccd74cce424
sierraki <sierrakiper@outlook.com>
2025-12-03 21:47:25 +0800

    20251202

---
+++ 力扣&Leetcode/算法&algorithm/题库/2533.好二进制字符串的数量.py
@@ -0,0 +1,18 @@
+class Solution:
+    def goodBinaryStrings(
+        self, minLength: int, maxLength: int, oneGroup: int, zeroGroup: int
+    ) -> int:
+        dp = [0] * (maxLength + 1)
+        for i in range(1, maxLength + 1):
+            tar1 = i - oneGroup
+            tar2 = i - zeroGroup
+            if i == oneGroup:
+                dp[i] += 1
+            if i == zeroGroup:
+                dp[i] += 1
+            if tar1 >= 0:
+                dp[i] += dp[tar1]
+            if tar2 >= 0:
+                dp[i] += dp[tar2]
+        res = sum(dp[minLength:]) % (10**9 + 7)
+        return res
