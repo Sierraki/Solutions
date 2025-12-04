fb21c34b89a5ee328750192ae1e0170501a50594
Sierraki <sierrakiper@outlook.com>
2025-12-04 10:58:12 +0800

    20251204

--- 力扣&Leetcode/数据库&Database/SQL/3764.Most Common Course Pairs.sql
+++ 力扣&Leetcode/数据库&Database/SQL/3764.Most Common Course Pairs.sql
@@ -1,50 +1,52 @@
 # Write your MySQL query statement below
-with
-    a1 as (
-        select
-            user_id,
-            avg(course_rating) avgg,
-            count(*) cnt
-        from
-            course_completions
-        group by
-            user_id
-    ),
-    a2 as (
-        select
-            user_id
-        from
-            a1
-        where
-            cnt >= 5
-            and avgg >= 4
-    ),
-    a3 as (
-        select
-            user_id id,
-            course_name c1,
-            lead(course_name) over (
-                partition by
-                    user_id
-                order by
-                    completion_date
-            ) c2
-        from
-            course_completions
-        where
-            user_id in (
-                select
-                    user_id
-                from
-                    a2
-            )
-    )
 select
     c1 first_course,
     c2 second_course,
     count(*) transition_count
 from
-    a3
+    (
+        (
+            select
+                user_id id,
+                course_name c1,
+                lead(course_name) over (
+                    partition by
+                        user_id
+                    order by
+                        completion_date
+                ) c2
+            from
+                course_completions
+            where
+                user_id in (
+                    select
+                        user_id
+                    from
+                        (
+                            (
+                                select
+                                    user_id
+                                from
+                                    (
+                                        (
+                                            select
+                                                user_id,
+                                                avg(course_rating) avgg,
+                                                count(*) cnt
+                                            from
+                                                course_completions
+                                            group by
+                                                user_id
+                                        )
+                                    ) t1
+                                where
+                                    cnt >= 5
+                                    and avgg >= 4
+                            )
+                        ) t2
+                )
+        )
+    ) t3
 where
     c2 is not null
 group by
