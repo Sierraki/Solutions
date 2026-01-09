60dddd6191b6117788ca9f402ab7ceba975e92b1
Sierraki <sierrakiper@outlook.com>
2026-01-09 22:04:52 +0800

    20260109

---
+++ Codeforces/1352A_Sum_of_Round_Numbers.py
@@ -0,0 +1,20 @@
+import sys
+
+
+def fun(num):
+    res = []
+    num = str(num)
+    cnt = 0
+    for i in range(len(num) - 1, -1, -1):
+        if num[i] != "0":
+            res.append(int(num[i]) * (10**cnt))
+        cnt += 1
+    return res
+
+
+n = int(input())
+for _ in range(n):
+    num = int(input())
+    ans = sorted(fun(num),reverse=True)
+    print(len(ans))
+    print(" ".join(map(str, ans)))
