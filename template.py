82240f9b490712375f45fbdf85258e3fcddbd9ff
Sierraki <sierrakiper@outlook.com>
2026-01-16 17:38:33 +0800

    20260115

---
+++ template.py
@@ -0,0 +1,9 @@
+import sys
+
+input = sys.stdin.readline
+# data = iter(sys.stdin.read().split())
+size = int(input())
+for _ in range(size):
+    n=int(input())
+    a,b=map(int,input().split())
+    nums=map(input().split())
