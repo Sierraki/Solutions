bc5982f2528781fb4f9683fa2449dba5759a07ca
Sierraki <sierrakiper@outlook.com>
2026-02-01 03:34:05 +0800

    20260201

---
+++ AtCoder/AtCoder Beginner Contest 441 (Promotion of Engineer Guild Fes)/D_Paid_Walk.py
@@ -0,0 +1,60 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def p(nums):
+    for i in nums:
+        print(i)
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def solve():
+    n, m, l, s, t = mii()
+    nums = [[] for _ in range(n + 1)]
+    for _ in range(m):
+        u, v, c = mii()
+        nums[u].append([v, c])
+    res = set()
+
+    def dfs(cur, path, cost):
+        if path > l or cost > t:
+            return
+        if (path) == l and s <= cost <= t:
+            res.add(cur)
+            return
+        for i, j in nums[cur]:
+            dfs(i, path + 1, cost + j)
+
+    dfs(1, 0, 0)
+    print(*sorted(list(res)))
+
+
+sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    # size = ii()
+    # for _ in range(size):
+    solve()
