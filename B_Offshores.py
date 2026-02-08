4c5923e67f103172d8c74c38bf6e3585c741ae77
Sierraki <sierrakiper@outlook.com>
2026-02-08 17:47:46 +0800

    20260208

---
+++ B_Offshores.py
@@ -0,0 +1,54 @@
+from collections import defaultdict, Counter, deque
+from math import sqrt, floor, gcd, ceil
+from bisect import bisect, bisect_left
+from itertools import accumulate as acc
+import sys
+
+input = sys.stdin.readline
+
+
+def mii():
+    return map(int, input().split())
+
+
+def lmii():
+    return list(map(int, input().split()))
+
+
+def ii():
+    return int(input())
+
+
+def si():
+    return input().strip()
+
+
+def lacc(nums):
+    return list(acc(nums))
+
+
+def ms(numss):
+    return "".join(map(str, numss))
+
+
+def solve():
+    n, x, y = mii()
+    nums = lmii()
+
+    cur = 0
+    mx = -1
+    for i in nums:
+        cnt = i // x
+        addd = cnt * y
+        cur += addd
+        res = i - addd
+        mx = max(res, mx)
+
+    print(cur + mx)
+
+
+# sys.setrecursionlimit(200000)
+if __name__ == "__main__":
+    size = ii()
+    for _ in range(size):
+        solve()
