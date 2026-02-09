21cd74625bc70925329a113d36c23b8109d8b305
Sierraki <sierrakiper@outlook.com>
2026-02-09 22:09:34 +0800

    20260209

--- Draf.py
+++ Draf.py
@@ -3,7 +3,6 @@ from math import sqrt, floor, gcd, ceil
 from bisect import bisect, bisect_left
 from itertools import accumulate
 import heapq
-import sys
 import copy
 
 
@@ -12,4 +11,18 @@ def p(numss):
         print(i)
 
 
-print('adasd')
+nums = [7, 2, 4]
+k = 2
+
+cur = deque([])
+ans = []
+
+for i, j in enumerate(nums):
+    if cur and cur[0]<=i-k:
+        cur.popleft()
+    while cur and nums[cur[-1]]<=j:
+        cur.pop()
+    cur.append(i)
+    if i>=k-1:
+        ans.append(nums[cur[0]])
+print(ans)
