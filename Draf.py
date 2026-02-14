011d00f402461305e9d9604724de6953d8a864d1
Sierraki <sierrakiper@outlook.com>
2026-02-14 14:55:23 +0800

    20260214

--- Draf.py
+++ Draf.py
@@ -2,7 +2,7 @@ from collections import defaultdict, Counter, deque
 from math import sqrt, floor, gcd, ceil
 from bisect import bisect, bisect_left
 from itertools import accumulate
-from functools import cache
+from functools import cache, lru_cache
 import heapq
 import copy
 
@@ -10,38 +10,3 @@ import copy
 def p(numss):
     for i in numss:
         print(i)
-
-
-n = 3
-k = 3
-
-vis = [False] * (n)
-res = []
-
-swap = True
-
-
-def dfs(path):
-    global swap
-    if swap:
-        if len(path) == n:
-            cur = "".join(path)
-            res.append(cur)
-            return
-        if len(res) == k:
-            swap = False
-            return
-
-        for i in range(n):
-            if not vis[i]:
-                path.append(str(i + 1))
-                vis[i] = True
-                dfs(path)
-                path.pop()
-                vis[i] = False
-    return
-
-
-dfs([])
-
-print(res[-1])
