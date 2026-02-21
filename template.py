b8670ea11bfef9b27a23759e6bb9dff7c09dbd96
Sierraki <sierrakiper@outlook.com>
2026-02-22 01:50:54 +0800

    update

--- template.py
+++ template.py
@@ -11,34 +11,22 @@ input = sys.stdin.readline
 
 def mii():
     return map(int, input().split())
-
-
 def lmii():
     return list(map(int, input().split()))
-
-
 def ii():
     return int(input())
-
-
 def si():
     return input().strip()
-
-
 def lacc(nums):
     return list(acc(nums))
-
-
 def ms(numss):
     return "".join(map(str, numss))
 
-
 def solve():
     pass
-
 
 # sys.setrecursionlimit(200000)
 if __name__ == "__main__":
     # size = ii()
     # for _ in range(size):
-    solve()
+        solve()
