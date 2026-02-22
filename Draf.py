e58808fe25269e80016d41cb574fa416bc62291c
Sierraki <sierrakiper@outlook.com>
2026-02-22 17:33:52 +0800

    20260222

--- Draf.py
+++ Draf.py
@@ -1,10 +1,4 @@
-import copy
-import heapq
-from bisect import bisect, bisect_left
-from collections import Counter, defaultdict, deque
-from functools import cache, lru_cache
-from itertools import accumulate
-from math import ceil, floor, gcd, sqrt
+from collections import Counter
 
 
 def p(numss):
@@ -12,16 +6,3 @@ def p(numss):
         print(i)
 
 
-n = 415
-
-def check(num):
-    ans = 1
-    for i in range(1, num + 1):
-        ans = ans * i
-    return ans
-ans = 0
-for i in str(n):
-    ans += check(int(i))
-    print(i, check(int(i)))
-
-print(Counter(list(str(n))) == Counter(list(str(ans))))
