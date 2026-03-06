99a804131a0ef9514de50f5dad3a26c7c45a4be5
Sierraki <sierrakiper@outlook.com>
2026-03-07 02:40:59 +0800

    20260307

--- Draf.py
+++ Draf.py
@@ -4,6 +4,9 @@ from collections import Counter, defaultdict, deque
 from functools import cache, lru_cache
 from itertools import accumulate as acc
 from math import ceil, floor, gcd, sqrt
+from operator import ne
+
+from matplotlib.pyplot import flag
 
 
 def p(numss):
@@ -11,6 +14,27 @@ def p(numss):
         print(i)
 
 
+s = "10001100101000000"
 
-n = 4
-k = 11
+s = list(map(int, s + s))
+
+
+
+nums = [1 if i % 2 == j else 0 for i, j in enumerate(s)]
+
+
+cur = 0
+l = 0
+k = len(s) // 2
+ans = float('inf')
+for r, val in enumerate(nums):
+    if r < k:
+        cur += val
+    else:
+        cur += val
+        cur -= nums[l]
+        l += 1
+        ans = min(ans, cur, k - cur)
+
+
+print(ans)
--- Leetcode/算法&Algorithm/题库/1888.使二进制字符串字符交替的最少反转次数.py
+++ Leetcode/算法&Algorithm/题库/1888.使二进制字符串字符交替的最少反转次数.py
@@ -1,25 +1,15 @@
 class Solution:
     def minFlips(self, s: str) -> int:
-        s = [int(i) for i in s]
-        k = n = len(s)  # 窗口长度
         s += s
-        t1 = [1, 0] * k
-        # print(s)
-        # print(t1)
-        a = []
-        for i in range(2 * n):
-            if s[i] == t1[i]:
-                a.append(-1)
-            else:
-                a.append(1)
-        # print(a)
-        cur = sum(a[:k])
-        mx = abs((sum(a[:k])))
-
-        for i in range(k, 2 * n):
-            cur += a[i] - a[i - k]
-            mx = max(mx, abs(cur))
-        # print(mx)
-        b = (k - mx) / 2
-        res = min(b, k - b)
-        return int(res)
+        nums = [1 if i % 2 == int(j) else 0 for i, j in enumerate(s)]
+        cur = 0
+        l = 0
+        k = len(s) // 2
+        ans = float("inf")
+        for r, val in enumerate(nums):
+            cur += val
+            if r >= k - 1:
+                ans = min(ans, cur, k - cur)
+                cur -= nums[l]
+                l += 1
+        return ans
