a12e8c3ca63f842a927dc3dfbe2413a2f0935531
Sierraki <sierrakiper@outlook.com>
2026-03-11 02:56:32 +0800

    20260311

--- Draf.py
+++ Draf.py
@@ -5,15 +5,10 @@ from functools import cache, lru_cache
 from itertools import accumulate as acc
 from math import ceil, floor, gcd, sqrt
 
-ratings = [1, 0, 2]
+n = 5
 
-res = [1] * len(ratings)
-for i in range(1, len(ratings)):
-    if ratings[i - 1] < ratings[i]:
-        res[i] = res[i - 1] + 1
-for i in range(len(ratings) - 2, -1, -1):
-    if ratings[i  ] > ratings[i+1]:
-        res[i]=max(res[i],res[i+1]+1)
-
-
-print(res)
+res = bin(n)[2:]
+l = n.bit_length()
+mask = (1 << l) - 1
+print(n ^ mask)
+print(mask)
--- Leetcode/算法&Algorithm/题库/1009.十进制整数的反码.py
+++ Leetcode/算法&Algorithm/题库/1009.十进制整数的反码.py
@@ -1,10 +1,5 @@
 class Solution:
     def bitwiseComplement(self, n: int) -> int:
-        res = bin(n)[2:]
-        ans = ""
-        for i in res:
-            if i == "1":
-                ans += "0"
-            else:
-                ans += "1"
-        return int(ans, 2)
+        if n == 0:
+            return 1
+        return n ^ ((1 << n.bit_length()) - 1)
